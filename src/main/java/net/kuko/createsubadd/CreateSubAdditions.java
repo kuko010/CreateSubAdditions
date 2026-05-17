fe5a9d75fc21c52451fb7a2326425385152dcf86
kuko010 <kukouwu@proton.me>
2026-05-17 13:41:38 +0200

    rename mod

---
+++ src/main/java/net/kuko/createsubadd/CreateSubAdditions.java
@@ -0,0 +1,44 @@
+package net.kuko.createsubadd;
+
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+
+// The value here should match an entry in the META-INF/neoforge.mods.toml file
+@Mod(CreateSubAdditions.MODID)
+public class CreateSubAdditions {
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "createsub_add";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public CreateSubAdditions(IEventBus modEventBus, ModContainer modContainer) {
+        modEventBus.addListener(this::commonSetup);
+
+
+
+        NeoForge.EVENT_BUS.register(this);
+        modEventBus.addListener(this::addCreative);
+//        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void commonSetup(FMLCommonSetupEvent event) {
+    }
+
+    // Add the example block item to the building blocks tab
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+    }
+
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+    }
+}
